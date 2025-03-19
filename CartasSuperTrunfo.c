11014e06da141ee152d4de4a328518f0c7fa9f62
thaymtins <thainammtins@gmail.com>
2025-03-19 23:24:21 +0000

    triunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,29 +3,46 @@
 
 
 int main() {
+    char estado2[50];
     char estado[50];
-    char codigo[50];
+    char codigo2[50];
+ char codigo1[50];
+    char cidade2[50];
     char cidade[50];
-    int populacao;
+    int populacao2[50];
+    int populacao[50];
     float area;
+    float area2;
     float pib;
+    float pib2;
     int  turistico;
+    int turistico2;
 printf("digite seu estado: \n");
 scanf("%s",estado);
-
+printf("digite seu estado2 \n");
+scanf("%s",estado2);
+printf("digite o codigo da carta2 \n");
+scanf("%s",codigo2);
 printf("digite o codigo da carta: \n");
-scanf("%s",codigo);
-
+scanf("%s",codigo1);
+printf("digite a cidade2: \n");
+scanf("%s",cidade2);
 printf("digite a cidade: \n");
-
+scanf("%s",cidade);
+printf("digite sua populacao2 \n");
+scanf("%d",populacao2);
 printf("digite sua populacao: \n");
 scanf("%d",populacao);
+printf("digite sua area2 \n");
+scanf("%f",area2);
 printf("digite sua area: \n");
 scanf("%f",area);
-printf("digite sua area: \n");
-scanf ("%f",area);
+printf("digite o pib2: \n");
+scanf("%f",pib2);
 printf("digite o pib: \n");
 scanf ("%f",pib);
+printf("digite o numero de ponto turistico2: \n");
+scanf("%d",turistico2);
 printf("digite o numero de ponto turistico: \n");
 scanf("%d",turistico);
 
